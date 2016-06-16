d7318bc5d978e3f1b8ee680c81c9481d21002a7a
Aaditya Kalsi <akalsi.dev@gmail.com>
2016-06-16 00:24:36 -0400

    Update 2

--- include/cppbitfield/bitfield.hpp
+++ include/cppbitfield/bitfield.hpp
@@ -8,14 +8,18 @@
 
 #include <cstdint>
 #include <type_traits>
+#include <typeinfo>
 
-#define DECLARE_BITFIELD_ENUM(X, Y, ...) \
+#define VA_ARGS_X(...) , ##__VA_ARGS__
+
+#define DECLARE_BITFIELD_ENUM_X(X, ...)  \
     enum class X                         \
     {                                    \
-        Y = 0,                           \
         __VA_ARGS__                      \
-        , NUM_FIELDS                     \
     }
+
+#define DECLARE_BITFIELD_ENUM(X, Y, ...) \
+    DECLARE_BITFIELD_ENUM_X(X, Y = 0  VA_ARGS_X(__VA_ARGS__), __NUM_FIELDS)
 
 namespace cppbitfield {
 
@@ -39,6 +43,69 @@ namespace cppbitfield {
         };
     };
 
+    template <int Idx, int... Sizes>
+    struct GetImpl;
+
+    template <int Idx>
+    struct GetImpl<Idx>
+    {
+        static const int value = -1;
+    };
+
+    template <int Idx, int S, int... Sizes>
+    struct GetImpl<Idx, S, Sizes...>
+    {
+        static const int value = (Idx == 0) ? S : GetImpl<Idx - 1, Sizes...>::value;
+    };
+
+
+    template <int Idx, int... Sizes>
+    struct SumTillImpl;
+
+    template <int Idx>
+    struct SumTillImpl<Idx>
+    {
+        static const int value = -1;
+    };
+
+    template <int Idx, int S, int... Sizes>
+    struct SumTillImpl<Idx, S, Sizes...>
+    {
+        static const int value = (Idx == 0) ? 0 : (S + SumTillImpl<Idx - 1, Sizes...>::value);
+    };
+
+    template <int... Sizes>
+    struct BitFieldSizes;
+
+    template <>
+    struct BitFieldSizes<>
+    {
+        static const int NumFields = 0;
+    };
+
+    template <int S, int... Sizes>
+    struct BitFieldSizes<S, Sizes...>
+    {
+        static_assert(S >= 1 && S <= 64, "Bit field size must be in the range [1, 64].");
+        static const int NumFields = 1 + sizeof...(Sizes);
+
+        template <int Idx>
+        struct Get
+        {
+            static_assert(Idx >= 0, "Index out of bounds.");
+            static_assert(Idx < (sizeof...(Sizes) + 1), "Index out of bounds.");
+            static const int value = GetImpl<Idx, S, Sizes...>::value;
+        };
+
+        template <int Idx>
+        struct SumTill
+        {
+            static_assert(Idx >= 0, "Index out of bounds.");
+            static_assert(Idx < (sizeof...(Sizes) + 1), "Index out of bounds.");
+            static const int value = SumTillImpl<Idx, S, Sizes...>::value;
+        };     
+    };
+
     template <class EnumType>
     struct BitFieldDescriptor
     {
@@ -53,7 +120,7 @@ namespace cppbitfield {
         template <IntType X>
         using AsEnum = typename AsEnumType<EnumType, IntType>::template Convert<X>;
 
-        static const IntType NumFields = AsInt<EnumType::NUM_FIELDS>::value;
+        static const IntType NumFields = AsInt<EnumType::__NUM_FIELDS>::value;
 
         template <EnumType fieldname, int fieldlen>
         struct Field
@@ -65,13 +132,39 @@ namespace cppbitfield {
         };
     };
 
-    template <class EnumType, class... Fields>
+    template <class EnumType, class Sizes>
     struct BitFields
     {
         using Traits = BitFieldDescriptor<EnumType>;
-        static_assert(sizeof...(Fields) == Traits::NumFields, "Number of fields must match total number of fields.");
+        
+        static const int NumFields = Traits::NumFields;
+
+        using IntType = typename Traits::IntType;
+
+        template <EnumType X>
+        using AsInt = typename Traits::template AsInt<X>;
+
+        template <IntType X>
+        using AsEnum = typename Traits::template AsEnum<X>;
+
+        template <int X>
+        using FieldLength = typename Sizes::template Get<X>;
+
+        template <int X>
+        using FieldOffset = typename Sizes::template SumTill<X>;
+
+        static_assert(Sizes::NumFields == NumFields, "Number of fields must match total number of fields.");
+        static_assert(FieldOffset<NumFields - 1>::value + FieldLength<NumFields - 1>::value <= 64, "Total number of bits must be <= 64.");
+        // static_assert(Sizes::SumTill<Sizes::NumFields - 1>) > 0, "Total number of bits must be <= 64.");
+
     };
 
 } // namespace cppbitfield
+
+#define DECLARE_BITFIELD_SIZES(X, ...) \
+    using X = cppbitfield::BitFieldSizes<__VA_ARGS__>;
+
+#define DECLARE_BITSET(N, X, Y) \
+    using N = cppbitfield::BitFields<X, Y>;
 
 #endif/*CPPBITFIELD_BITFIELD_HPP*/
--- test/tBitfields.cpp
+++ test/tBitfields.cpp
@@ -7,20 +7,45 @@
 
 #include <cppbitfield/bitfield.hpp>
 
-using namespace cppbitfield;
-
-CPP_TEST( SingleElementEnum )
+CPP_TEST( t0 )
 {
     DECLARE_BITFIELD_ENUM(
-        Foo,
-        FOO);
+         FooEnum,
+               A,
+               B,
+               C);
 
-    using FieldMaker = BitFieldDescriptor<Foo>;
+    TEST_TRUE(static_cast<int>(FooEnum::A) == 0);
 
-    using MyBitfield = BitFields<
-                        Foo,
-                        FieldMaker::Field<Foo::FOO, 64>>;
+    DECLARE_BITFIELD_SIZES(
+        FooSizes,
+               1,
+               2,
+               3);
 
-    MyBitfield X;
-    static_cast<void>(&X);
+    TEST_TRUE(FooSizes::Get<0>::value == 1);
+    TEST_TRUE(FooSizes::Get<1>::value == 2);
+    TEST_TRUE(FooSizes::Get<2>::value == 3);
+    TEST_TRUE(FooSizes::SumTill<0>::value == 0);
+    TEST_TRUE(FooSizes::SumTill<1>::value == 1);
+    TEST_TRUE(FooSizes::SumTill<2>::value == 3);
+
+    DECLARE_BITSET(Foo, FooEnum, FooSizes);
+
+    Foo x;
+    static_cast<void>(&x);
+
+    TEST_TRUE(Foo::NumFields == 3);
+
+    TEST_TRUE(Foo::FieldOffset<0>::value == 0);
+    TEST_TRUE(Foo::FieldOffset<1>::value == 1);
+    TEST_TRUE(Foo::FieldOffset<2>::value == 3);
+
+    TEST_TRUE(Foo::FieldLength<0>::value == 1);
+    TEST_TRUE(Foo::FieldLength<1>::value == 2);
+    TEST_TRUE(Foo::FieldLength<2>::value == 3);
+
+    TEST_TRUE(Foo::AsEnum<0>::value == FooEnum::A);
+    TEST_TRUE(Foo::AsEnum<1>::value == FooEnum::B);
+    TEST_TRUE(Foo::AsEnum<2>::value == FooEnum::C);
 }
